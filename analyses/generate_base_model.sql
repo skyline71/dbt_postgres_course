22715f979b6210618ec6082e3a9167412d698e9e
skyline71 <romanmozzherin2015@gmail.com>
2025-07-01 15:45:24 +0300

    codegen package

---
+++ analyses/generate_base_model.sql
@@ -0,0 +1,5 @@
+{{ codegen.generate_base_model(
+    source_name='demo_src',
+    table_name='flights',
+    materialized='table'
+) }}
---
+++ analyses/generate_model_yaml.sql
@@ -0,0 +1,3 @@
+{{ codegen.generate_model_yaml(
+    model_names=['stg_flights__flights', 'stg_flights__ticket_flights']
+) }}
